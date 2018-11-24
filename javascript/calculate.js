25b3c3d330e009d7f8a03f07a90065fb8bbdbcc7
Ross <RossHall251@outlook.com>
2018-11-24 15:49:18 -0500

    Assignment 7 code

--- javascript/calculate.js
+++ javascript/calculate.js
@@ -3,12 +3,18 @@
 	ross_hall@student.uml.edu
 	UMass Lowell Computer Science Student
 	COMP 4610 GUI Programming 1
-	Assignment No.6: Creating an Interactive Dynamic Table
-    11/15/2018
+	Assignment No.7: Using the jQuery Validation Plugin with Your Dynamic Table
+    11/24/2018
     
     Description: This is the javascript file for the dynamic
-    multiplication table website. The main function is run 
-    every time the user clicks the calculate table button. 
+    multiplication table website. This version uses the jQuery 
+    plugin validate.js to perform realtime validation. 
+    The main function is run every time the user clicks the
+    calculate table button as long as the inputs are valid.
+    One last validation check is performed in the main function
+    limiting the number of cells of the table to keep run time
+    reasonably fast. This validation is performed in the main function
+    after submission because it is dependant on all four input values.  
 */
 const CELL_LIMIT = 99999;
 const NUMBER_LIMIT = Number.MAX_SAFE_INTEGER
@@ -18,42 +24,106 @@ const X_LO = 0;
 const X_HI = 1;
 const Y_LO = 2;
 const Y_HI = 3;
+// enable eager evaluation
+
+// enable validation on focusout
+$.validator.setDefaults({
+    onfocusout: function (element) {
+        $(element).valid();
+    }
+});
+
+$().ready(function(){
+    /* Form validation */
+    $("#inputForm").validate({
+        submitHandler: function() {
+     //       $('#required').addClass('hide');
+            main();
+        },
+        errorElement: "div",
+        rules: {
+            field1: {
+                required: true,
+                number: true,
+                step: 1,
+                max: NUMBER_LIMIT,
+                min: -NUMBER_LIMIT
+            },
+            field2: {
+                required: true,
+                number: true,
+                step: 1,
+                max: NUMBER_LIMIT,
+                min: -NUMBER_LIMIT
+            },
+            field3: {
+                required: true,
+                number: true,
+                step: 1,
+                max: NUMBER_LIMIT,
+                min: -NUMBER_LIMIT
+            },
+            field4: {
+                required: true,
+                number: true,
+                step: 1,
+                max: NUMBER_LIMIT,
+                min: -NUMBER_LIMIT
+            }
+        },
+        messages: {
+            field1: {
+               required: "An integer must be specfied for every bound.",
+                number: "Please specify a valid integer. Ex: -10000",
+                step: "Please specify a valid integer. Ex: -10000",
+                max: "Please enter an integer less than " + Number.MAX_SAFE_INTEGER + ".",
+                min: "Please enter an integer greater than " + -Number.MAX_SAFE_INTEGER + "."
+            },
+            field2: {
+                required: "An integer must be specified for every bound.",
+                number: "Please specify a valid integer. Ex: -10000",
+                step: "Please specify a valid integer. Ex: -10000",
+                max: "Please enter an integer less than " + Number.MAX_SAFE_INTEGER + ".",
+                min: "Please enter an integer greater than " + -Number.MAX_SAFE_INTEGER + "."
+            },
+            field3: {
+                required: "An integer must be specified for every bound.",
+                number: "Please specify a valid integer. Ex: -10000",
+                step: "Please specify a valid integer. Ex: -10000",
+                max: "Please enter an integer less than " + Number.MAX_SAFE_INTEGER + ".",
+                min: "Please enter an integer greater than " + -Number.MAX_SAFE_INTEGER + "."
+            },
+            field4: {
+                required: "An integer must be specified for every bound.",
+                number: "Please specify a valid integer. Ex: -10000",
+                step: "Please specify a valid integer. Ex: -10000",
+                max: "Please enter an integer less than " + Number.MAX_SAFE_INTEGER + ".",
+                min: "Please enter an integer greater than " + -Number.MAX_SAFE_INTEGER + "."
+            }
+        }
+    });
+});
 
 function main() {
-    let table_holder = document.getElementById("tableHolder");
-    let messageSpan = document.getElementById("messageSpan");
-    let badForms = document.getElementsByClassName("badForm");
-    clearPrevious(table_holder, messageSpan, badForms); 
+    /* Valid inputs, perform cell limit check */
+    /* Clear previous errors */
+    $('.badForm').removeClass("badForm");
+    $('#multError').text("");
     let error_exists = false;
 
     formNodes = [];  // array to hold entire input nodes
-    formNodes.push(document.getElementById("lowerX"));
-    formNodes.push(document.getElementById("upperX"));
-    formNodes.push(document.getElementById("lowerY"));
-    formNodes.push(document.getElementById("upperY"));
+    formNodes.push($("#lowerX"));
+    formNodes.push($("#upperX"));
+    formNodes.push($("#lowerY"));
+    formNodes.push($("#upperY"));
     
     formValues = [];  // array to hold values in input fields
     let tempValue; 
     for (let i = 0; i < 4; i++) {
-        let str_length = formNodes[i].value.length;
-        console.log(str_length);
-        tempValue = Number(formNodes[i].value)
+        tempValue = Number(formNodes[i].val())
         formValues.push(tempValue);
-        if (Number.isNaN(tempValue) || tempValue % 1 !== 0 || str_length === 0) {
-            /* Invalid integer */
-            formNodes[i].classList.add('badForm');
-            messageSpan.innerText = "A bound must be an integer value. Ex: 10000";
-            messageSpan.classList.add('error');
-            error_exists = true;
-        } else if (tempValue < -NUMBER_LIMIT || tempValue > NUMBER_LIMIT) {
-            /* Integer out of valid range */
-            formNodes[i].classList.add('badForm');
-            messageSpan.innerText = "A bound must a value between -" + NUMBER_LIMIT + " and +" + NUMBER_LIMIT; 
-            messageSpan.classList.add('error');
-            error_exists = true;
-        }
     }
-    if (!error_exists) {
+//    if (!error_exists) {
         let temp_node;
         if (formValues[0] > formValues[1]) {
             /* Order x bounds by size */
@@ -76,32 +146,35 @@ function main() {
             formNodes[3] = temp_node;
         }
         /* arrays now have form [xLowBound, xHighBound, yLowBound, yHighBound] */
-        let x_size = Math.abs(formValues[X_HI]) - Math.abs(formValues[X_LO]) + 1;
-        let y_size = Math.abs(formValues[Y_HI]) - Math.abs(formValues[Y_LO]) + 1; 
+        let x_size = Math.abs(formValues[X_HI] - formValues[X_LO]) + 1;
+        let y_size = Math.abs(formValues[Y_HI] - formValues[Y_LO]) + 1; 
+
         if (x_size * y_size > CELL_LIMIT) {
             /* Too many cells in table */
-            formNodes[X_LO].classList.add('badForm');
-            formNodes[X_HI].classList.add('badForm');
-            formNodes[Y_LO].classList.add('badForm');
-            formNodes[Y_HI].classList.add('badForm');
-            messageSpan.innerText = "The table is limited to displaying " + CELL_LIMIT + " products. Please decrease the size of the ranges."
-            messageSpan.classList.add('error');
-            error_exists = true;
-        } else if (multiplicationDoesOverflow(formValues[X_HI], formValues[Y_HI])) {
-            /* Overflow occurs for some multiplications */
-            formNodes[X_HI].classList.add('badForm');
-            formNodes[Y_HI].classList.add('badForm');
-            messageSpan.innerText = "The product of numbers in this range is too large to represent! Please enter smaller bounds."
-            messageSpan.classList.add('error');
-            error_exists = true;
+            formNodes[X_LO].addClass('badForm');
+            formNodes[X_HI].addClass('badForm');
+            formNodes[Y_LO].addClass('badForm');
+            formNodes[Y_HI].addClass('badForm');
+            $('#multError').text("The table is limited to displaying " + CELL_LIMIT + " products. Please decrease the size of the ranges.");
+            $('#multError').addClass('error');
+            error_exists = true;        
         }
-    }
+        /*overflow check not needed (number displayed as string in scientific notation*/
+        /* else if (multiplicationDoesOverflow(formValues[X_HI], formValues[Y_HI])) {
+            /* Overflow occurs for some multiplications */
+         /*   formNodes[X_HI].classList.add('badForm');
+            formNodes[Y_HI].classList.add('badForm');
+            multError.innerText = "The product of numbers in this range is too large to represent! Please enter smaller bounds."
+            multError.classList.add('error');
+            error_exists = true;
+}*/
+    
     if (!error_exists) {
         /************************************************************************
             Valid inputs, create table
         *************************************************************************/
-        messageSpan.innerText = 'Click on a cell to display the equation.';
-        messageSpan.classList.remove('error');
+        $('#scroller').remove(); // clear previous table
+        $('#messageSpan').text('Click on a cell to display the equation.');
         scroller = document.createElement('div');
         scroller.setAttribute('id', 'scroller');
         theTable = document.createElement("table");
@@ -135,43 +208,32 @@ function main() {
             theTable.appendChild(t_row);
         }
         scroller.appendChild(theTable);
-        table_holder.appendChild(scroller);
+        $('#tableHolder').append(scroller);
     }
 }
 
 /***********************************************************************
  * Helper Functions
 **********************************************************************/
-function clearPrevious(table_holder, messageSpan, badForms) {
-    /* Clear table or previous error from last run */
-    while (table_holder.firstChild) {
-        table_holder.removeChild(table_holder.firstChild);
-    }
-    while (badForms.length) {
-        badForms[0].classList.remove("badForm");
-    }
-    messageSpan.textContent = "";
-}
-
 let selectTableCell = function () {
+    td_selected = $('.selected');
+    /* unselect any selected cells */
+    td_selected.removeClass('selected');
+    
     /* displays cell equation in messageSpan*/
-    td_selected = document.getElementsByClassName('selected');
-    while (td_selected.length) {
-        /* unselect any selected cells */
-        td_selected[0].classList.remove('selected');
-    }
     this.classList.add('selected');
     let calc_string = this.getAttribute('id') + ' = ' + this.innerText;
-    messageSpan.innerText = calc_string;
+    $('#messageSpan').text(calc_string);
 }
 
-// This function was taken from:
+//overflow function not needed, large vals represents as string with e 
+// This function was taken from: 
 // https://www.algotech.solutions/blog/javascript/handle-number-overflow-javascript/
-function multiplicationDoesOverflow(a, b) {
+//function multiplicationDoesOverflow(a, b) {
     /* Returns true if overflow occurs during multiplication */
-    if (a === 0 || b === 0) {
+/*    if (a === 0 || b === 0) {
         return false;
     }  
     var c = a * b;
     return a !== c/b || b !== c/a;
-}
+}*/
